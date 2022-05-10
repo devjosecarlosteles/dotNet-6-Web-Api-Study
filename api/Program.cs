8415605a99139563b8f0572f60e77872a9bbf797
devjosecarlosteles <devjosecarlosteles@gmail.com>
2022-05-09 23:41:19 -0300

    feat: endpoint user get

--- api/Program.cs
+++ api/Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("user", (HttpResponse response) => response.StatusCode = 200);
 
 app.Run();
